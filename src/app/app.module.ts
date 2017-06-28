8e7204b0462e468c42b1b942e93d99be1088e72d
johnbt <johnbt@outlook.com>
2017-06-28 13:29:23 +0200

    uif-textfield added w/label

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { UifTextfieldComponent } from './uif-textfield/uif-textfield.component';
+import { uiftextfieldComponent } from './uif';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UifTextfieldComponent
+    AppComponent, 
+    uiftextfieldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/uif/uiftextfield.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+declare var fabric: any;
+
+@Component({
+    selector: 'uif-textfield',
+    templateUrl: './uiftextfield.component.html',
+    styleUrls: ['./uiftextfield.component.css']
+})
+export class uiftextfieldComponent implements OnInit {   
+    @Input() uifLabel: string;
+
+    constructor() { }
+
+    createTextFields() {
+        var TextFieldElements = document.querySelectorAll(".ms-TextField");
+        for (var i = 0; i < TextFieldElements.length; i++) {
+            new fabric['TextField'](TextFieldElements[i]);
+        }
+    }
+
+    ngOnInit() {
+        this.createTextFields();
+     }
+}
+
