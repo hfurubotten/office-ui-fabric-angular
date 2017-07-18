d09294a49d048a35bceaa7872db7f7704583a932
johnbt <johnbt@outlook.com>
2017-07-18 11:37:11 +0200

    label component completed

--- src/app/uif/uiflabel.component.ts
+++ src/app/uif/uiflabel.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit, Input } from '@angular/core';
     templateUrl: './uiflabel.component.html'
 })
 export class UifLabelComponent implements OnInit {
+    @Input() uifDisabled: boolean = false;
+    @Input() uifRequired: boolean = false;
     constructor() { }
 
     ngOnInit() { }
