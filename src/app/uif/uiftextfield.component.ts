f291b675a15ca5b49f20da8451dc36aecb9c61d9
johnbt <johnbt@outlook.com>
2017-07-26 14:16:37 +0200

    refactoring

--- src/app/uif/uiftextfield.component.ts
+++ src/app/uif/uiftextfield.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, AfterViewInit } from '@angular/core';
 import 'office-ui-fabric-js/src/components/TextField/TextField';
 
 @Component({
@@ -7,29 +7,41 @@ import 'office-ui-fabric-js/src/components/TextField/TextField';
     templateUrl: './uiftextfield.component.html',
 })
 
-export class UifTextFieldComponent implements AfterViewInit {
+export class UifTextFieldComponent implements OnInit, AfterViewInit {
     @Input() uifId: string = '';
     @Input() uifLabel: string = '';
     @Input() uifPlaceholder: string = '';
     @Input() uifUnderlined: boolean = false;
     @Input() uifMultiline: boolean = false;
     @Input() uifDisabled: boolean = false;
-    @Input() uifType: string = 'Text';
+    @Input() uifPassword: boolean = false;
     @Input() value: string = '';
     @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
     private textfield: fabric.TextField;
+    private textfieldElement: HTMLElement;
+    inputType: string = 'Text';
 
     constructor() { }
 
     private initialize(): void {
         if (this.uifId != null) {
-            var textfieldElement = document.getElementById(this.uifId);
-            var textfield = new fabric.TextField(textfieldElement);
+            this.textfieldElement = document.getElementById(this.uifId);
+            var textfield = new fabric.TextField(this.textfieldElement);
+        }
+    }
+
+    private setTypePassword(): void {
+        if (this.uifPassword === true) {
+            this.inputType = 'Password';
         }
     }
 
     onValueChanged() {
         this.valueChange.emit(this.value);
+    }
+
+    ngOnInit() {
+        this.setTypePassword();
     }
 
     ngAfterViewInit() {
