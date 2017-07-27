2d5b8245d42589fefdb7c26504ef35ec149a97fb
johnbt <johnbt@outlook.com>
2017-07-27 09:46:19 +0200

    added component: spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,16 +19,26 @@ export class AppComponent implements OnInit {
   displayPersistentPanel: boolean = false;
   singleCheckboxChecked: boolean = false;
   checkboxValues: string;
-  
+  spinSpinners: boolean = true;
+  hideSpinners: boolean = false;
+
   singleCheckboxClicked(value: boolean) {
     this.singleCheckboxChecked = value;
   }
 
   choiceFieldGroupValuesChanged(values: CheckboxData[]) {
     this.checkboxValues = '';
-    for(let i = 0; i < values.length; i++) {
+    for (let i = 0; i < values.length; i++) {
       this.checkboxValues += '{value: ' + values[i].value + '} {checked: ' + values[i].checked + '} {disabled: ' + values[i].disabled + '}\n';
     }
+  }
+
+  stopSpinners() {
+    this.spinSpinners = this.spinSpinners === true ? false : true;
+  }
+
+  showSpinners() {
+    this.hideSpinners = this.hideSpinners === true ? false : true;
   }
 
   button1Click() {
@@ -97,41 +107,41 @@ export class AppComponent implements OnInit {
     alert("persistent overlay clicked...");
   }
 
-  
 
-  dropdownValues: {value: string, text: string}[] = [
-    {'value': 'mø', 'text': 'moo'},
-    {'value': 'mjau', 'text': 'meow'},
-    {'value': 'voff', 'text': 'woof'}
+
+  dropdownValues: { value: string, text: string }[] = [
+    { 'value': 'mø', 'text': 'moo' },
+    { 'value': 'mjau', 'text': 'meow' },
+    { 'value': 'voff', 'text': 'woof' }
   ];
   dropdownSelected = this.dropdownValues[1].value;
   dropdownPlaceholder = 'Please select a sound...';
   dropdownPlaceHolderSelected = '-1';
-  
 
-  radioValuesWithDisabled: {value: string, text: string, disabled: boolean}[] = [
-    {'value': 'banan', 'text': 'Banana', 'disabled': false},
-    {'value': 'eple', 'text': 'Apple', 'disabled': false},
-    {'value': 'druer', 'text': 'Grapes', 'disabled': true},
-    {'value': 'ananas', 'text': 'Pineapple', 'disabled': false}
+
+  radioValuesWithDisabled: { value: string, text: string, disabled: boolean }[] = [
+    { 'value': 'banan', 'text': 'Banana', 'disabled': false },
+    { 'value': 'eple', 'text': 'Apple', 'disabled': false },
+    { 'value': 'druer', 'text': 'Grapes', 'disabled': true },
+    { 'value': 'ananas', 'text': 'Pineapple', 'disabled': false }
   ];
-    radioValues: {value: string, text: string}[] = [
-    {'value': 'grønn', 'text': 'Green'},
-    {'value': 'rød', 'text': 'Red'},
-    {'value': 'blå', 'text': 'Blue'},
-    {'value': 'gul', 'text': 'Yellow'}
+  radioValues: { value: string, text: string }[] = [
+    { 'value': 'grønn', 'text': 'Green' },
+    { 'value': 'rød', 'text': 'Red' },
+    { 'value': 'blå', 'text': 'Blue' },
+    { 'value': 'gul', 'text': 'Yellow' }
   ];
 
   radioLabel: string = 'Select color:'
   radioSelected = this.radioValues[1].value;
   radioSelectedFruit = '';
-  
+
   messageBarText = 'Some random, medium length text for the message bar here. Feel free to add <i>html</i> formatting as well. Some random, medium length text for the message bar here. Feel free to add <i>html</i> formatting as well';
-  
-  constructor() {}
+
+  constructor() { }
 
   ngOnInit() {
-    
+
   }
 
 }
