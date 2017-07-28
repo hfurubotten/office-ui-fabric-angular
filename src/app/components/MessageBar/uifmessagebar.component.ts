7c6478c9c783f0969a698e85ae798f70264e8d00
johnbt <johnbt@outlook.com>
2017-07-28 09:09:57 +0200

    fix memory leak in messagebar

--- src/app/components/MessageBar/uifmessagebar.component.ts
+++ src/app/components/MessageBar/uifmessagebar.component.ts
@@ -10,45 +10,38 @@ export class UifMessageBarComponent implements OnInit {
     @Input() uifLinkLabel: string = '';
     @Input() uifLinkURL: string = '';
     @Input() uifType: string = '';
-    baseClass: string = 'ms-MessageBar';
-    extendedClass: string = ' ms-MessageBar--';
-    iconBaseClass: string = 'ms-Icon ms-Icon--';
+    public iconCssClass: string;
     
-    constructor() { }
-
-    addLinkToContent():void {
+    private addLinkToContent():void {
          if(this.uifLinkLabel != '' && this.uifLinkURL != '') {
              this.uifContent += '<br/>' + '<a href="' + this.uifLinkURL + '" class="ms-Link">' + this.uifLinkLabel + '</a>';
          }
     }
 
-    getCssClass():string {
-        var cssClass = this.baseClass;
-        if(this.uifType != '') {
-            cssClass += this.extendedClass + this.uifType;
+    private setIconCssClass(): void {
+        let baseClass = 'ms-Icon--';
+        switch(this.uifType) {
+            case 'success':
+                this.iconCssClass = baseClass + 'Completed';
+                break;
+            case 'error':
+                this.iconCssClass = baseClass + 'ErrorBadge';
+                break;
+            case 'blocked':
+                this.iconCssClass = baseClass + 'Blocked';
+                break;
+            case 'severeWarning':
+                this.iconCssClass = baseClass + 'Warning';
+                break;
+            default:
+                this.iconCssClass = baseClass + 'Info';
+                break;
         }
-        return cssClass;
     }
 
-    getCssIcon():string {
-        var cssClass = this.iconBaseClass + 'Info';
-
-        if(this.uifType == 'success') {
-            cssClass = this.iconBaseClass + 'Completed';
-        }
-        if(this.uifType == 'error') {
-            cssClass = this.iconBaseClass + 'ErrorBadge';
-        }
-        if(this.uifType == 'blocked') {
-            cssClass = this.iconBaseClass + 'Blocked';
-        }
-        if(this.uifType == 'severeWarning') {
-            cssClass = this.iconBaseClass + 'Warning';
-        }
-
-        return cssClass;        
+    public ngOnInit() {
+        this.setIconCssClass();
+        this.addLinkToContent();
     }
-
-    ngOnInit() {}
 
 }
