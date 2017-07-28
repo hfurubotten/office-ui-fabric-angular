7a83659b7830f9bacabdcc43f54c7fec07c8261a
johnbt <johnbt@outlook.com>
2017-07-28 10:01:19 +0200

    dropdown refactoring

--- src/app/components/Dropdown/uifdropdown.component.ts
+++ src/app/components/Dropdown/uifdropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, AfterViewInit } from '@angular/core';
 import 'office-ui-fabric-js/src/components/Dropdown/Dropdown';
 
 @Component({
@@ -7,37 +7,35 @@ import 'office-ui-fabric-js/src/components/Dropdown/Dropdown';
     templateUrl: './uifdropdown.component.html',
 })
 
-export class UifDropdownComponent implements OnInit {
+export class UifDropdownComponent implements OnInit, AfterViewInit {
     @Input() uifLabel: string = '';
     @Input() uifDisabled: boolean = false;
-    @Input() uifOptions: {value: string, text: string}[];
+    @Input() uifOptions: { value: string, text: string }[];
     @Input() uifSelectedValue: string = '';
     @Input() uifPlaceholder: string = '';
     @Input() uifId: string = '';
-    @Output() uifSelectedValueChange:EventEmitter<string> = new EventEmitter<string>();
+    @Output() uifSelectedValueChange: EventEmitter<string> = new EventEmitter<string>();
     private dropdown: fabric.Dropdown;
-    uifSelected: {value: string, text: string} = {'value':'', 'text':''};
+    public uifSelected: { value: string, text: string } = { 'value': '', 'text': '' };
 
-    constructor() { }
-
-    private createDropdown():void {
-        if(this.uifId != null) {
+    private initialize(): void {
+        if (this.uifId !== null && this.uifId !== undefined) {
             var dropdownElement = document.getElementById(this.uifId);
             this.dropdown = new fabric.Dropdown(dropdownElement);
         }
     }
 
-    onValueChanged(target: HTMLSelectElement):void {
+    public onValueChanged(target: HTMLSelectElement): void {
         this.uifSelectedValue = target.value;
         this.uifSelectedValueChange.emit(this.uifSelectedValue);
     }
 
-    ngOnInit() {
+    public ngOnInit() {
         this.uifSelected = this.uifOptions.filter(o => o.value == this.uifSelectedValue)[0];
     }
 
-     ngAfterViewInit() {
-        this.createDropdown();
-     }
+    public ngAfterViewInit() {
+        this.initialize();
+    }
 
 }
