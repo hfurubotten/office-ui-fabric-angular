93efd51ca5bcda786a46a69927af3f814c7d146e
johnbt <johnbt@outlook.com>
2017-08-04 09:09:00 +0200

    add pivot component base

---
+++ src/app/components/Pivot/uifpivot.component.ts
@@ -0,0 +1,12 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'uif-pivot',
+    templateUrl: './uifpivot.component.html'
+})
+export class UifPivotComponent implements AfterViewInit {
+
+    public ngAfterViewInit() {
+    }
+}
